82b4a29927ee384e8d7278e80fe04bf422f40ce8
unknown <boureimatapily1@gmail.com>
2020-07-03 19:28:07 +0000

    ADD new components

---
+++ src/Components/Chat/NewRoomForm.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { withStyles } from "@material-ui/styles";
+import { Grid, Container } from '@material-ui/core';
+
+const styles = {
+    root:{
+        marginTop:10,
+    },
+}
+
+class NewRoomForm extends React.Component{
+ 
+  render(){
+     const { classes } = this.props;
+    return(
+        <Container fixed> 
+            <Grid container className={classes.root}>
+                 
+             </Grid>
+               
+        </Container>
+    )
+  }
+}
+export default  withStyles(styles)(NewRoomForm)
--- src/Pages/EX.js
+++ src/Pages/EX.js
@@ -21,3 +21,30 @@ export default function EX() {
 
 ///// class base component
 
+
+
+import React from 'react'
+import { withStyles } from "@material-ui/styles";
+import { Grid, Container } from '@material-ui/core';
+
+const styles = {
+    root:{
+        marginTop:10,
+    },
+}
+
+class Chat extends React.Component{
+ 
+  render(){
+     const { classes } = this.props;
+    return(
+        <Container fixed> 
+            <Grid container className={classes.root}>
+                 
+             </Grid>
+               
+        </Container>
+    )
+  }
+}
+export default  withStyles(styles)(Chat)
